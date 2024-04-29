a8f7e5f9df6af908b732cebcea69c91bb38c1127
noahgairing <46930496+noahgairing@users.noreply.github.com>
2024-04-29 12:41:00 +0100

    Added simple Status Handler class

---
+++ handlers/statusHandler.py
@@ -0,0 +1,28 @@
+import os
+import config
+import json
+
+class StatusHandler:
+    def __init__(self, pubMedID: str):
+        self.pubMedID = pubMedID
+        self.filePath = os.path.join(config.getStatusFolderPath(), f"{id}.json")
+        
+        if not os.path.isfile(self.filePath):
+            self.status = {}
+            self.__saveStatus()
+            return
+        
+        with open(self.filePath, "r") as file:
+            self.status = json.load()
+            
+    def get(self):
+        return self.status
+    
+    def set(self, newStatus):
+        self.status = newStatus
+        self.__saveStatus()
+            
+    def __saveStatus(self):
+        with open(self.filePath, "w") as file:
+            json.dump(file, self.status, indent=4)
+            
