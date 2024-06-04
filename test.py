634cd3dfdaaaebcdc988af9ce1f686d085a575dd
noahgairing <46930496+noahgairing@users.noreply.github.com>
2024-06-04 17:40:02 +0100

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from llms import LLMHandler
-
-s = LLMHandler(messageHistory=[{"role": "user", "content": "My name jamal"}, {
-    "role": "assistant", "content": "hi jamal"
-}])
-
-print("I most definitely know that" + s.askWithRetry("what is my name", textToComplete="I most definitely know that"))
-
-print(s.ask("summarise every message of this conversation so far please"))
